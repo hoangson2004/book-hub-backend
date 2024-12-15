b489c040b063f23d4925686599ba34f91a85544c
hoangson2004 <lehoangson022004@gmail.com>
2024-12-16 04:41:57 +0700

    connect db

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./db'); 
+
+dotenv.config(); 
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
